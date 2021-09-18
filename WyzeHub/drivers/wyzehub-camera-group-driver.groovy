3287a1663ff1c0e8e5ae0a6313e5d8b2b68ff8a0
fieldsjm <39744574+fieldsjm@users.noreply.github.com>
2021-09-17 22:14:26 -0400

    Create wyzehub-camera-group-driver.groovy

---
+++ WyzeHub/drivers/wyzehub-camera-group-driver.groovy
@@ -0,0 +1,121 @@
+/*
+ * Import URL: 
+ *
+ * DON'T BE A DICK PUBLIC LICENSE
+ *
+ * Version 1.1, December 2016
+ *
+ * Copyright (C) 2021 Jake Lehner
+ * 
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document.
+ * 
+ * DON'T BE A DICK PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 1. Do whatever you like with the original work, just don't be a dick.
+ * 
+ *    Being a dick includes - but is not limited to - the following instances:
+ *
+ *    1a. Outright copyright infringement - Don't just copy this and change the name.
+ *    1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+ *    1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+ *
+ * 2. If you become rich through modifications, related works/services, or supporting the original work,
+ *    share the love. Only a dick would make loads off this work and not buy the original work's
+ *    creator(s) a pint.
+ * 
+ * 3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
+ *    you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
+ *
+ */
+
+import groovy.transform.Field
+
+public static String version() {  return "v1.0.5"  }
+
+public String deviceModel() { return 'WYZEC1-JZ' }
+
+public String groupTypeId() { return 1 }
+
+metadata {
+	definition(
+		name: "WyzeHub Camera Group", 
+		namespace: "jakelehner", 
+		author: "Jake Lehner", 
+		importUrl: ""
+	) {
+		capability "Outlet"
+		capability "Refresh"
+    }
+}
+
+void installed() {
+    app = getApp()
+	logDebug("installed()")
+
+	refresh()
+	initialize()
+}
+
+void updated() {
+    app = getApp()
+	logDebug("updated()")
+    initialize()
+}
+
+void initialize() {
+    app = getApp()
+	logDebug("initialize()")
+}
+
+void parse(String description) {
+	app = getApp()
+	logWarn("Running unimplemented parse for: '${description}'")
+}
+
+def refresh() {
+	getChildDevices().each { device -> 
+		device.refresh()
+	}
+}
+
+def on() {
+	getChildDevices().each { device -> 
+		device.on()
+	}
+}
+
+def off() {
+	getChildDevices().each { device -> 
+		device.off()
+	}
+}
+
+private getApp() {
+	app = getParent()
+	while(app && app.name != "WyzeHub") {
+		app = app.getParent()
+	}
+	return app
+}
+
+private void logDebug(message) {
+	app = getApp()
+	app.logDebug("[${device.label}] " + message)
+}
+
+private void logInfo(message) {
+	app = getApp()
+	app.logInfo("[${device.label}] " + message)
+}
+
+private void logWarn(message) {
+	app = getApp()
+	app.logWarn("[${device.label}] " + message)
+}
+
+private void logError(message) {
+	app = getApp()
+	app.logError("[${device.label}] " + message)
+}
