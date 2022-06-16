f8ecad2cfc675968172cb6aa7861f602f53542ea
fieldsjm <39744574+fieldsjm@users.noreply.github.com>
2022-06-16 15:08:19 -0400

    Update wyzehub-camera-driver.groovy

--- WyzeHub/drivers/wyzehub-camera-driver.groovy
+++ WyzeHub/drivers/wyzehub-camera-driver.groovy
@@ -29,6 +29,8 @@
  *    you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
  *
  * Release Notes:
+ *   v1.5 - Polling for event detection (motion, sound, smoke alarm, co alarm)
+ *   v1.4 - Floodlight support added
  *   v1.2 - Event Recording Enable/Disable
  *   v1.1 - Initial Driver Release. 
  *        - Support for Power On/Off, Notification Preferences
@@ -70,6 +72,24 @@ public String deviceModel() { return device.getDataValue('product_model') ?: 'WY
 @Field static final String wyze_property_device_floodlight_value_on = '1'
 @Field static final String wyze_property_device_floodlight_value_off = '2'
 
+@Field static final String wyze_event_motion_1 = '1'
+@Field static final String wyze_event_motion_2 = '6'
+@Field static final String wyze_event_motion_3 = '7'
+@Field static final String wyze_event_motion_4 = '13'
+@Field static final String wyze_event_sound = '2'
+@Field static final String wyze_event_smoke = '4'
+@Field static final String wyze_event_co = '5'
+
+@Field static final String wyze_event_tag_person = '101'
+@Field static final String wyze_event_tag_face = '101001'
+@Field static final String wyze_event_tag_vehicle = '102'
+@Field static final String wyze_event_tag_pet = '103'
+@Field static final String wyze_event_tag_package = '104'
+@Field static final String wyze_event_tag_crying = '800001'
+@Field static final String wyze_event_tag_barking = '800002'
+@Field static final String wyze_event_tag_meowing = '800003'
+
+
 metadata {
 	definition(
 		name: "WyzeHub Camera", 
@@ -77,17 +97,23 @@ metadata {
 		author: "Jake Lehner", 
 		importUrl: "https://raw.githubusercontent.com/jakelehner/Hubitat/master/WyzeHub/drivers/wyzehub-camera-driver.groovy"
 	) {
-		capability "Outlet"
+		capability "CarbonMonoxideDetector"
+        capability "MotionSensor"
+        capability "Outlet"
 		capability "Refresh"
+        capability "SmokeDetector"
+        capability "SoundSensor"
 		capability "Switch"
-
-		attribute "motion_recording", "bool"
-        	attribute "notifications_enabled", "bool"
-        	attribute "motion_notification", "bool"
-        	attribute "sound_notification", "bool"
-			attribute "floodlight_powerstate", "bool"
-		attribute "online", "bool"
         
+        attribute "motion_recording", "enum", ["true","false"]
+        attribute "notifications_enabled", "enum", ["true","false"]
+        attribute "motion_notification", "enum", ["true","false"]
+        attribute "sound_notification", "enum", ["true","false"]
+        attribute "floodlight_powerstate", "enum", ["true","false"]
+		attribute "online", "enum", ["true","false"]
+        attribute "objects_detected", "string"  //any AI detected objects (person, package, vehicle, pet, etc)
+        
+        command "settingsRefresh"
         command(
              "setMotionRecording", 
              [
@@ -146,25 +172,31 @@ metadata {
 	}
 
 	preferences {
-        input "pollInterval", "number", title: "Polling Interval", description: "Change polling frequency (in seconds)", defaultValue:240, range: "10..86400", required: true, displayDuringSetup: true
-        input "debugOutput", "bool", title: "Enable debug logging", defaultValue: true
+        input "SETTING_DEVICE_POLL", "number", title: "Device Polling Interval", description: "Change polling frequency of settings (minutes); 0 to disable polling", defaultValue:5, required: true, displayDuringSetup: true
+        input "SETTING_EVENT_POLL", "number", title: "Event Polling Interval", description: "Change polling frequency of events (seconds); 0 to disable polling", defaultValue:120, required: true, displayDuringSetup: true
+        input "SETTING_EVT_VARIANCE", "number", title: "Time since Event", description: "Input desired time since last event to be considered active (minutes)", defaultValue:1, required: true, displayDuringSetup: true
+        input "SETTING_INACTIVE_TIME", "number", title: "Time to Deactivate Events", description: "Input desired time since last event to automatically deactivate (seconds)", defaultValue:30, required: true, displayDuringSetup: true
 	}
 }
 
 void installed() {
-    logDebug "installed()"
+    logWarn("installed()")
+    sendEvent(name: 'switch', value: 'off')
+    sendEvent(name: 'motion', value: 'inactive')
     initialize()
 }
 
 void updated() {
-    if (debugOutput) runIn(1800,logsOff) //disable debug logs after 30 min
-    log.debug "updated()"
+    def msg = 'updated'
+    if(SETTING_DEVICE_POLL == 0) msg += ", disabling polling for device settings"
+    if(SETTING_EVENT_POLL == 0) msg += ", disabling polling for events"
+    logWarn(msg)
     initialize()
 }
 
 void initialize() {
-    if (debugOutput) log.debug "initialize()"
-    unschedule('refresh')
+    logWarn("initialize()")
+    settingsRefresh()
     refresh()
 }
 
@@ -172,14 +204,30 @@ void parse(String description) {
     logWarn("Running unimplemented parse for: '${description}'")
 }
 
-def refresh() {
+//Returns device settings (properties)
+def settingsRefresh() {
+    unschedule('settingsRefresh')
     app = getApp()
-	logInfo("Refresh Device")
+	logInfo("Refresh Device Data")
 	app.apiGetDevicePropertyList(device.deviceNetworkId, deviceModel()) { propertyList ->
 		createDeviceEventsFromPropertyList(propertyList)
 	}
+    if(SETTING_DEVICE_POLL != 0) {
+        def devicePoll = SETTING_DEVICE_POLL*60
+        runIn(devicePoll, settingsRefresh)
+    }
+}
 
-	runIn(pollInterval, refresh)
+//Returns most recent recording event (search is limited to last 24 hours), known camera events motion, sound, smoke alarm, CO alarm
+def refresh() {
+    unschedule('refresh')
+    app = getApp()
+	logInfo("Refreshing Recorded Events")
+    app.apiGetDeviceEventList(device.deviceNetworkId) { eventList ->
+		createDeviceEventsFromEventList(eventList)
+	}
+
+	if(SETTING_EVENT_POLL != 0) runIn(SETTING_EVENT_POLL, refresh)
 }
 
 def on() {
@@ -199,7 +247,7 @@ def off() {
 def setMotionRecording(motion_record) {
     app = getApp()
     logInfo("setMotionRecording() Pressed")
-    if (debugOutput) logDebug('Setting Motion Recording to ' + motion_record)
+    logDebug('Setting Motion Recording to ' + motion_record)
     
     wyze_action = motion_record == "true" ? wyze_action_motion_on : wyze_action_motion_off
 
@@ -210,7 +258,7 @@ def setMotionRecording(motion_record) {
 
 def setAllNotifications(all_notify) {
     logInfo("setAllNotifications() Pressed")
-    if (debugOutput) logDebug('Setting All Notifications to ' + all_notify)
+    logDebug('Setting All Notifications to ' + all_notify)
     
     value = all_notify == "true" ? wyze_property_device_notifications_value_true : wyze_property_device_notifications_value_false
 	id = wyze_property_notifications
@@ -222,45 +270,44 @@ def setAllNotifications(all_notify) {
 
 def setMotionNotification(motion_notify) {
     logInfo("setMotionNotification() Pressed")
-    if (debugOutput) logDebug('Setting Motion Notifications to ' + motion_notify)
+    logDebug('Setting Motion Notifications to ' + motion_notify)
     
     value = motion_notify == "true" ? wyze_property_device_motion_notify_value_true : wyze_property_device_motion_notify_value_false
 	id = wyze_property_motion_notify
     
     sendProperty(id, value)
-    runIn(pollInterval,refresh)
 }
 
 def setSoundNotification(sound_notify) {
 	logInfo("setSoundNotification() Pressed")
-    if (debugOutput) logDebug('Setting Sound Notifications to ' + sound_notify)
+    logDebug('Setting Sound Notifications to ' + sound_notify)
     
     value = sound_notify == "true" ? wyze_property_device_sound_notify_value_true : wyze_property_device_sound_notify_value_false
 	id = wyze_property_sound_notify
 
     sendProperty(id, value)
-    runIn(pollInterval,refresh)
-}	
+}
+
 def setFloodlightPowerstate(floodlight_powerstate) {
-logInfo("setFloodlightPowerstate() Pressed")
-if (debugOutput) logDebug('Setting floodlight powerstate to ' + floodlight_powerstate)
-
-value = floodlight_powerstate == "true" ? wyze_property_device_floodlight_value_on : wyze_property_device_floodlight_value_off
-id = wyze_property_floodlight
-
-sendProperty(id, value)
-runIn(pollInterval,refresh)
-
+    logInfo("setFloodlightPowerstate() Pressed")
+    logDebug('Setting floodlight powerstate to ' + floodlight_powerstate)
+    
+    value = floodlight_powerstate == "true" ? wyze_property_device_floodlight_value_on : wyze_property_device_floodlight_value_off
+    id = wyze_property_floodlight
+    
+    sendProperty(id, value)
 }
 
 private sendProperty(pid, pvalue) {
     app = getApp()
     app.apiSetDeviceProperty(device.deviceNetworkId, deviceModel(), pid, pvalue)
+    
+    runIn(10, settingsRefresh)
 }
     
 void createDeviceEventsFromPropertyList(List propertyList) {
 	app = getApp()
-    if (debugOutput) logDebug(propertyList)
+    //logDebug(propertyList)
     String eventName, eventUnit
     def eventValue // could be String or number
 
@@ -284,7 +331,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
 				}
 
 				if (device.currentValue(eventName) != eventValue) {
-					if (debugOutput) logDebug("Updating Property 'switch' to ${eventValue}")
+					logDebug("Updating Property 'switch' to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -296,7 +343,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
                 eventValue = propertyValue == wyze_property_device_online_value_true ? "true" : "false"
                 
 				if (device.currentValue(eventName) != eventValue) {
-					if (debugOutput) logDebug("Updating Property 'online' to ${eventValue}")
+					logDebug("Updating Property 'online' to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -308,7 +355,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
                 eventValue = propertyValue == wyze_property_device_notifications_value_true ? "true" : "false"
 
 				if (device.currentValue(eventName) != eventValue) {
-                    if (debugOutput) logDebug("Updating Property ${eventName} to ${eventValue}")
+                    logDebug("Updating Property ${eventName} to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -320,7 +367,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
                 eventValue = propertyValue == wyze_property_device_motion_record_value_true ? "true" : "false"
 
 				if (device.currentValue(eventName) != eventValue) {
-                    if (debugOutput) logDebug("Updating Property ${eventName} to ${eventValue}")
+                    logDebug("Updating Property ${eventName} to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -332,7 +379,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
                 eventValue = propertyValue == wyze_property_device_motion_notify_value_true ? "true" : "false"
 
 				if (device.currentValue(eventName) != eventValue) {
-                    if (debugOutput) logDebug("Updating Property ${eventName} to ${eventValue}")
+                    logDebug("Updating Property ${eventName} to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -344,7 +391,7 @@ void createDeviceEventsFromPropertyList(List propertyList) {
                 eventValue = propertyValue == wyze_property_device_sound_notify_value_true ? "true" : "false"
 
 				if (device.currentValue(eventName) != eventValue) {
-					if (debugOutput) logDebug("Updating Property ${eventName} to ${eventValue}")
+					logDebug("Updating Property ${eventName} to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
@@ -356,13 +403,147 @@ void createDeviceEventsFromPropertyList(List propertyList) {
 				eventValue = propertyValue == wyze_property_device_floodlight_value_on ? "true" : "false"
 
 				if (device.currentValue(eventName) != eventValue) {
-					if (debugOutput) logDebug("Updating Property ${eventName} to ${eventValue}")
+					logDebug("Updating Property ${eventName} to ${eventValue}")
 					app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 				}
             break
 
         }
     }
+}
+
+void createDeviceEventsFromEventList(List eventList) {
+	app = getApp()
+    //logDebug(eventList)
+    String eventName, eventUnit, eventValue
+    
+    def eventTime = eventList.event_ts[0]
+    def eventType = eventList.event_value[0] 
+    
+    def eventTag = eventList.tag_list[0]
+    def eventTagCount = eventTag.size()
+    def eventTagDesc = ""
+
+    def now = (new Date()).getTime()
+    def elapsedTime = now - eventTime
+    
+    def evtVariance = SETTING_EVT_VARIANCE ? SETTING_EVT_VARIANCE*60000 : 60000 // 1 min, default (written in ms)
+    def timeToInactive = SETTING_INACTIVE_TIME ? SETTING_INACTIVE_TIME : 30 // 30 sec default (written in s)
+
+    //if(elapsedTime < 1800000) {
+    if(elapsedTime < evtVariance) {
+        eventUnit = null
+        
+        switch(eventType) {
+            case wyze_event_motion_1:
+            case wyze_event_motion_2:
+            case wyze_event_motion_3:
+            case wyze_event_motion_4:
+            eventName = "motion"
+            eventValue = "active"
+            app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+            runIn(timeToInactive,autoInactiveMotion)
+            break
+            case wyze_event_sound:
+            eventName = "sound"
+            eventValue = "detected"
+            app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+            runIn(timeToInactive,autoInactiveSound)
+            break
+            case wyze_event_smoke:
+            eventName = "smoke"
+            eventValue = "detected"
+            app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+            runIn(timeToInactive,autoInactiveSmoke)
+            break
+            case wyze_event_co:
+            eventName = "carbonMonoxide"
+            eventValue = "detected"
+            app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+            runIn(timeToInactive,autoInactiveCO)
+            break
+            default:
+                logWarn("Event Type ${eventType} not found")
+            break
+        }
+
+        if (eventTagCount > 0) {
+            for(x=0;x<eventTagCount;x++){
+                def tag = eventTag[x]
+                if(x>0) eventTagDesc += ', '
+                
+                switch(tag) {
+                    case wyze_event_tag_person:
+                    eventTagDesc += "Person"
+                    break
+                    case wyze_event_tag_face:
+                    eventTagDesc += "Face"
+                    break
+                    case wyze_event_tag_vehicle:
+                    eventTagDesc += "Vehicle"
+                    break
+                    case wyze_event_tag_pet:
+                    eventTagDesc += "Pet"
+                    break
+                    case wyze_event_tag_package:
+                    eventTagDesc += "Package"
+                    break
+                    case wyze_event_tag_crying:
+                    eventTagDesc += "Baby Crying"
+                    break
+                    case wyze_event_tag_barking:
+                    eventTagDesc += "Dog Barking"
+                    break
+                    case wyze_event_tag_meowing:
+                    eventTagDesc += "Meowing"
+                    break
+                    default:
+                        logWarn("Unknown event tag: ${eventTag}")
+                    break
+                }
+            }
+            eventName = "objects_detected"
+            eventValue = eventTagDesc
+            app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+        }
+        
+        logInfo("${eventTagDesc ?: ''} ${eventName} detected")
+    }
+}    
+
+//Automatically deactive events after user selected time period
+void autoInactiveMotion() {
+    eventName = "motion"
+    eventValue = "inactive"
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+    if (device.currentValue("objects_detected") != "None") autoObjNone()
+}
+
+void autoInactiveSound() {
+    eventName = "sound"
+    eventValue = "not detected"
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+    if (device.currentValue("objects_detected") != "None") autoObjNone()
+}
+
+void autoInactiveSmoke() {
+    eventName = "smoke"
+    eventValue = "clear"
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+    if (device.currentValue("objects_detected") != "None") autoObjNone()
+}
+
+void autoInactiveCO() {
+    eventName = "carbonMonoxide"
+    eventValue = "clear"
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
+    if (device.currentValue("objects_detected") != "None") autoObjNone()
+}
+
+void autoObjNone() {
+    eventName = "objects_detected"
+    eventValue = "None"
+    app.doSendDeviceEvent(device, eventName, eventValue, eventUnit)
 }
 
 private getApp() {
@@ -391,9 +572,4 @@ private void logWarn(message) {
 private void logError(message) {
 	app = getApp()
 	app.logError("[${device.label}] " + message)
-}
-
-def logsOff(){
-    log.warn "debug logging disabled..."
-    device.updateSetting("debugOutput",[value:"false",type:"bool"])
 }
